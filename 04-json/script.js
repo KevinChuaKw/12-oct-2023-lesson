ca049a1dfd4a3e3712c0bd174a770c0d2907d226
Kevin Chua <Kevin_chua3@hotmail.com>
2023-10-24 13:20:40 +0000

    Completing 04-json

--- 04-json/script.js
+++ 04-json/script.js
@@ -13,11 +13,19 @@
 document.addEventListener("DOMContentLoaded", async function() {
     // await is to tell a function 
     // to pause at the line until that line 
-    const response = await loadData; 
+    const tasks = await loadData();
+    for (let t of tasks){
+        const listItem = document.createElement(`li`);
+        listItem.innerHTML = `${t.name} due on ${t.date} (${t.urgency})`; 
+
+        const todoList = document.querySelector("#todoList");
+        todoList.appendChild(listItem); 
+    }
 }); 
 
+// loadData will basically read in the JSON object
+// in the `data.json` and return it
 async function loadData(){
     const response = await axios.get("data/data.json");
-    return response.data
-    console.log(response.data); 
+    return response.data; 
 }
