fb0a94f19ca57ac47aa7f285af0d286223d3c864
Kevin Chua <Kevin_chua3@hotmail.com>
2023-10-28 11:00:44 +0000

    Doing 11 layer groups

---
+++ 11-layer-groups/script.js
@@ -0,0 +1,28 @@
+const map = L.map('map'); 
+
+// Set the centerpoint at zoo
+// 1.4047, 103.7949
+map.setView([1.4047, 103.7949]);
+
+// need a tile layer 
+L.tileLayer
+
+// A layer - is one marker, one circle (aka one overlay on 
+// A layer group can organise layers 
+
+
+// Base layers: you can only show one at a time 
+
+
+// Overlays: can show as many as you want
+
+
+
+// When the variable is pural - it is an array 
+// When the variable is singular - it is an object
+
+
+
+// Refactor code - taking code that is already working and putting them into other functions 
+
+
