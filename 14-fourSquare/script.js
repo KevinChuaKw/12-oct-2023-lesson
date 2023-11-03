caeba200d590123584a6a283c7760bd93cbfc5d3
Kevin Chua <Kevin_chua3@hotmail.com>
2023-11-03 11:50:29 +0000

    setting up full example for industry project

---
+++ 14-fourSquare/script.js
@@ -0,0 +1,22 @@
+// Closure - Only happens with an anonymous function, for example  
+
+// When the element is created the function would remember the input that was provided just for the this particular instance
+
+// const nums = [101.103,105,107]
+// const funcs = []; 
+// for (let n of numbers){
+//     funcs.push(function(){
+        // the value of n must be a local scope variable
+//         console.log(n); 
+//     })
+// }
+// funcs[1](); 
+
+// it closes on the value n and this has to be a local scope variable. 
+
+
+// Refractoring - to take code that is already working and tiding it up
+// Programming is getting the code to work 
+// Software engining is setting the structure of the code and organising the files that you hav 
+
+// The bind pop up can return a string / HTML / function
