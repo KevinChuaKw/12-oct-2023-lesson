58112500c4f2ef00467abb0a1ebc598327c9cf92
Kevin Chua <Kevin_chua3@hotmail.com>
2023-11-08 13:24:30 +0000

    doing up simple map

---
+++ 07-simple-map/script.js
@@ -0,0 +1,6 @@
+// L is an object representing `Leaflet`
+// It is available whne we inclide `Leafet.js`
+// .map function creates a map object 
+// the first arugment is the ID of the element to contain the map
+const mapObject = L.map("mapContainer"); 
+
